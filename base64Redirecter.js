a0483b9863f9ac1265aa9e03a19e3bd105c9125b
Jeremy Banks <jeremy@jeremybanks.com>
2011-02-16 17:13:27 -0500

    Foo

---
+++ base64Redirecter.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+var fs = require("fs"),
+    
+    extensionMimes = {
+        html: "text/html",
+        js: "application/javascript",
+    
+        unknown: "application/octet-stream"
+    },
+
+    mimeFromFilename = function(filename) {
+        var dotIndex = filename.lastIndexOf("."),
+            extension = dotIndex !== -1 ? filename.substr(dotIndex + 1) : null;
+            mime = extensionMimes[extension];
+        
+        return mime || extensionMimes.unknown;
+    },
+    
+    filename = process.argv[2],
+    
+    redirectingHTML = function(url) {
+        return "<!doctype html><html><head><meta charset=\"utf-8\">" + 
+            "<title>Redirecting...</title><meta http-equiv=\"refresh\" " +
+            "content=\"0;url=" + url + "\"><body>Redirecting...";
+    };
+
+if (filename) {
+    console.log(redirectingHTML(
+        "data:" + mimeFromFilename(filename) +
+        ";base64," + fs.readFileSync(filename).toString("base64")));
+} else {
+    console.log(["Usage:", process.argv[0], process.argv[1], "file"].join(" "));
+};
--- scriptPager.js
+++ scriptPager.js
@@ -4,7 +4,7 @@ var util = require("util"),
     path = require("path"),
 
     htmlTemplate = "<!doctype html><head><meta charset=\"utf-8\"><title>" +
-        "</title></head><body><noscript>JavaScript is required.</noscript>",
+        "</title><body><noscript>JavaScript is required.</noscript>",
 
     requireCode = "var require = function(filename) {" +
         "if (! (filename in require.loaded)) {" +
