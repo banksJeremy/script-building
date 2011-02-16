64044de5acf27c87837c9f0794530781d0eb809c
Jeremy Banks <jeremy@jeremybanks.com>
2011-02-16 17:33:47 -0500

    Stopped using alert() in example.

--- example/foo.js
+++ example/foo.js
@@ -1,3 +1,4 @@
 exports.bar = function() {
-    alert("Hello World");
+    $("body").append($("<p>", { css: { font: "300% Bold Georgia" },
+                                text: "Hello World" }));
 };
